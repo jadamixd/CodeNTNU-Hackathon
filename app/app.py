164cf428c32fbb12935b4b0cbefb0049956dc2a1
Magnus Giverin <magnusgiverin@icloud.com>
2023-10-28 16:58:11 +0200

    Info side på plass

--- app/app.py
+++ app/app.py
@@ -12,21 +12,24 @@ def save_image():
         image_binary = base64.b64decode(image_data.split(',')[1])
 
         # Define the directory to save the images
-        image_directory = 'snapshots/'
+        image_directory = 'app/static/'
+        
         # Save the image to a file in the specified directory
-        
-        print(image_directory + 'reciept.jpg')
-        
         with open(image_directory + 'reciept.jpg', 'wb') as f:
             f.write(image_binary)
-
+        
         return 'Image saved successfully', 200
+    
     except Exception as e:
         return 'Failed to save the image: ' + str(e), 500
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/information')
+def information():
+    return render_template('information.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
