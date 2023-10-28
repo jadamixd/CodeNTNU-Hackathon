bd0331ce0a51e15a4f0bb9fd98590d92e1c59a71
Magnus Giverin <magnusgiverin@icloud.com>
2023-10-28 14:45:50 +0200

    OGså for Jakob

--- app/app.py
+++ app/app.py
@@ -7,20 +7,5 @@ app.static_folder = 'static'
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-    
-    # Call a function from the database_checker script to check the credentials
-    is_valid = True
-    
-    if is_valid:
-        # Redirect to a success page
-        return "Login successful"
-    else:
-        # Redirect to a failure page or show an error message
-        return "Login failed"
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
