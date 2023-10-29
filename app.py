1723ab9f950e46704f7f92ba440a90d8246347fb
Jakob Gundersen <jasevgu@gmail.com>
2023-10-29 11:49:30 +0100

    temp

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask, render_template, request
 import base64
 import sys
 
-app = Flask(__name__)
-
 # Add the root directory to sys.path
 # sys.path.append('/main')
 
 # Now you can import main
-from main import main
 
 app = Flask(__name__)
+from main import main
 
 @app.route('/save-image', methods=['POST'])
 def save_image():
@@ -21,7 +19,7 @@ def save_image():
         image_binary = base64.b64decode(image_data.split(',')[1])
 
         # Define the directory to save the images
-        image_directory = 'static/'
+        image_directory = '/static/'
         
         # Save the image to a file in the specified directory
         with open(image_directory + 'reciept.jpg', 'wb') as f:
@@ -38,9 +36,8 @@ def index():
 
 @app.route('/information')
 def information():
-    data = main()
-    print(data)
-    return render_template('information.html')
+    data = ["a", "b"]
+    return render_template('information.html', data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True)
